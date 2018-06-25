48a50c1d5b028afead2193cea837e18f3bb7c86d
garima29 <dadheechgarima371@gmail.com>
2018-06-25 18:44:14 +0530

    Added channel manager slider on home page

--- index.php
+++ index.php
@@ -580,14 +580,14 @@
 							<p>Connecting you with maximum number of OTAs</p>
 						</div>
 					   <section class="customer-logos slider">
-						  <div class="slide"><img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"></div>
-						  <div class="slide"><img src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg"></div>
-						  <div class="slide"><img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"></div>
-						  <div class="slide"><img src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg"></div>
-						  <div class="slide"><img src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg"></div>
-						  <div class="slide"><img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg"></div>
-						  <div class="slide"><img src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg"></div>
-						  <div class="slide"><img src="https://image.freepik.com/free-vector/retro-label-on-rustic-background_82147503374.jpg"></div>
+						  <div class="slide channel-logo"><img src="assets/images/channel/1.png"></div>
+						  <div class="slide channel-logo"><img src="assets/images/channel/5.png"></div>
+						  <div class="slide channel-logo"><img src="assets/images/channel/3.png"></div>
+						  <div class="slide channel-logo"><img src="assets/images/channel/2.png"></div>
+						  <div class="slide channel-logo"><img src="assets/images/channel/4.png"></div>
+						  <div class="slide channel-logo"><img src="assets/images/channel/6.png"></div>
+						  <div class="slide channel-logo"><img src="assets/images/channel/7.png"></div>
+						  <div class="slide channel-logo"><img src="assets/images/channel/8.png"></div>
 					   </section>
 					</div>
 				</div>
