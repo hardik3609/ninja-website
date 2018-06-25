4a7d5f2fb9fdf392b52259049d862920070496e3
garima29 <dadheechgarima371@gmail.com>
2018-06-25 13:04:20 +0530

    Corrected screenshot spell

--- index.php
+++ index.php
@@ -223,7 +223,7 @@
 			<div class="row">
 				<div class="col-lg-12">
 					<div class="center-heading">
-						<h2 class="section-title">Screen Shoot</h2>
+						<h2 class="section-title">Screen Shots</h2>
 					</div>
 				</div>
 				<div class="offset-lg-3 col-lg-6">
